9f36a0228b2b9b498a118d57361429dd9e4ee683
Sayoni <sayoni.sait@maqta.ae>
2022-05-17 13:36:04 +0400

    implemented ActivityIndicator

--- src/users/UserList.js
+++ src/users/UserList.js
@@ -1,14 +1,14 @@
 
 import * as React from 'react';
 import { useEffect } from 'react';
-import { View, Text, Button, FlatList, StyleSheet } from 'react-native';
+import { View, Text, Button, FlatList, StyleSheet, ActivityIndicator } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import UserListItem from './UserListItem';
 import { fetchUsers } from './usersSlice';
 
 
 const UserListScreen = ({ navigation }) => {
-    const users = useSelector(state => state.users.data)
+    const users = useSelector(state => state.users)
     const dispatch = useDispatch()
 
     useEffect(() => {
@@ -17,21 +17,28 @@ const UserListScreen = ({ navigation }) => {
 
     return (
         <View style={styles.root} >
-            <FlatList
-                showsHorizontalScrollIndicator={false}
-                data={users}
-                keyExtractor={(result) => result.login.uuid}
-                renderItem={({ item }) =>
-                    <UserListItem item={item}></UserListItem>
-                }
-            ></FlatList>
+            {users.fetching ? <ActivityIndicator /> :
+                <FlatList
+                    showsHorizontalScrollIndicator={false}
+                    data={users.data}
+                    keyExtractor={(result) => result.login.uuid}
+                    renderItem={({ item }) =>
+                        <UserListItem item={item}></UserListItem>
+                    }
+                    ListEmptyComponent={<Text style={{ alignSelf: 'center', padding: 10 }}>Something went wrong</Text>}
+                ></FlatList>}
+
+
+
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     root: {
-        marginBottom: 10
+        marginBottom: 10,
+        paddingVertical: 10,
+        justifyContent: 'center'
     },
     title: {
         fontWeight: 'bold',
